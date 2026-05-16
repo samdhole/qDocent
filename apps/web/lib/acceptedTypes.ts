dbc188251a078591dc6cb1e0256d8b3fbebbacf7
Test User <test@example.com>
2026-05-16 19:51:03 -0400

    feat: add NOTEBOOK_ACCEPT constant to lib/acceptedTypes.ts

---
+++ apps/web/lib/acceptedTypes.ts
@@ -0,0 +1,5 @@
+export const NOTEBOOK_ACCEPT: Record<string, Array<string>> = {
+  "application/pdf": [".pdf"],
+  "application/vnd.openxmlformats-officedocument.wordprocessingml.document": [".docx"],
+  "application/vnd.openxmlformats-officedocument.presentationml.presentation": [".pptx"],
+};
